71cbeafce356a4b848d5167b04823a1bba295b21
Christian Pari <ChristianPari@outlook.com>
2020-09-04 19:14:46 -0400

    Python Challeneges

---
+++ print_function.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+
+print(*range(1, input() + 1), sep="")
+
+# if i understood correctly, importing "print_function" allows you to put more in depth code into a print statement/makes it act as a function and a print
